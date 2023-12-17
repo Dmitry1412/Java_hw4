fdf1ebb1bd324082aebe0c01a0ddb2ce85895a25
Dmitry <sicily007@rambler.ru>
2023-12-17 17:46:05 +0300

    hw_sem5

---
+++ src/sem5/ex3/Printer.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+// sort -> heapSort
+// n -> sortLength
+class HeapSort {
+
+    public static void heapSort(int[] sortArray, int sortLength) {
+// Введите свое решение ниже
+        // Построение кучи (перегруппируем массив)
+        for (int i = sortLength / 2 - 1; i >= 0; i--)
+            heapify(sortArray, sortLength, i);
+
+        // Один за другим извлекаем элементы из кучи
+        for (int i=sortLength-1; i>=0; i--)
+        {
+            // Перемещаем текущий корень в конец
+            int temp = sortArray[0];
+            sortArray[0] = sortArray[i];
+            sortArray[i] = temp;
+
+            // Вызываем процедуру heapify на уменьшенной куче
+            heapify(sortArray, i, 0);
+        }
+    }
+    static void heapify(int arr[], int n, int i)
+    {
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int l = 2*i + 1; // левый = 2*i + 1
+        int r = 2*i + 2; // правый = 2*i + 2
+
+        // Если левый дочерний элемент больше корня
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        // Если самый большой элемент не корень
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(arr, n, largest);
+        }
+    }
+}
+
+// Не удаляйте и не меняйте метод Main!
+public class Printer {
+    public static void main(String[] args) {
+        int[] initArray;
+
+        if (args.length == 0) {
+            initArray = new int[]{17, 32, 1, 4, 25, 17, 0, 3, 10, 7, 64, 1};
+        } else {
+            initArray = Arrays.stream(args[0].split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        System.out.println("Initial array:");
+        System.out.println(Arrays.toString(initArray));
+        HeapSort.heapSort(initArray, initArray.length);
+        System.out.println("Sorted array:");
+        System.out.println(Arrays.toString(initArray));
+    }
+}
